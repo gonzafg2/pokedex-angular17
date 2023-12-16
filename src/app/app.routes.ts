9d4adde8fd0e093f60d6dc5d85399e067b7c826a
Gonzalo Fleming <gonzafg2@gmail.com>
2023-12-16 02:02:08 -0300

    add 404 view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Page not found',
   },
 ];
