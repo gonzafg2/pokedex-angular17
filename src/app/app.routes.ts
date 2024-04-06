4371427e8c385c97f9c667df047ba6a10d576b09
Gonzalo Fleming <gaflemingg@falabella.cl>
2024-04-06 03:40:22 -0300

    refactor: :tada: Let template for activity

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
-import { NotFoundComponent } from './views/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page',
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
-    title: 'Page not found',
   },
 ];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../components/header/header.component';
-import { CardPokeComponent } from '../../components/card-poke/card-poke.component';
-import { FailPokeComponent } from '../../components/fail-poke/fail-poke.component';
 import { PokeapiService } from '../../services/pokeapi.service';
 import { Pokemon } from '../../models/pokedex.model';
-import { LoaderComponent } from '../../components/loader/loader.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    CardPokeComponent,
-    FailPokeComponent,
-    LoaderComponent,
-    FormsModule,
-  ],
+  imports: [HeaderComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -25,8 +16,6 @@ export class HomeComponent {
   public subtitle: string = 'Based on PokeAPI';
   public pokemonName: string = 'Mew';
   public pokemon: null | Pokemon = null;
-  public POKEAPI_URL_BASE: string = 'https://pokeapi.co/api/v2/pokemon/';
-  public loading: boolean = true;
   public visible: boolean = false;
 
   constructor(private pokeapiService: PokeapiService) {}
@@ -41,7 +30,6 @@ export class HomeComponent {
 
   public searchPokemon(): void {
     const pokeName = this.pokeNameSanitize(this.pokemonName);
-    this.loading = true;
 
     this.pokeapiService.getPokemon(pokeName).subscribe({
       next: (data: any) => {
@@ -54,12 +42,10 @@ export class HomeComponent {
           type: data.types[0].type.name,
           weight: data.weight,
         };
-        this.loading = false;
       },
       error: (e) => {
         console.log(e);
         this.pokemon = null;
-        this.loading = false;
       },
     });
   }
